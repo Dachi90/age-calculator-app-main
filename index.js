60b0348e6e6b56dd376d15c10423cf7b9c96e164
Martin PCescritorio <bergesio90@gmail.com>
2023-05-09 21:00:31 +0200

    algunos errores

--- index.js
+++ index.js
@@ -6,23 +6,51 @@ $Submit = document.querySelector('.submit');
 $YearSpan = document.querySelector('.span-year');
 $MonthSpan = document.querySelector('.span-month');
 $DaySpan = document.querySelector('.span-day');
-console.log($YearInput, $MonthInput, $DayInput);
 
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
+const daysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 $YearInput.setAttribute('max', `${currentYear}`);
 
-$Submit.addEventListener('click', () => {
+const calculateAge = () => {
 	const userDate = new Date(`${$YearInput.value}-${$MonthInput.value}-${$DayInput.value}`);
-	console.log(userDate);
-	const diferencia = currentDate.getTime() - userDate.getTime();
-	const milisegundosEnDia = 1000 * 60 * 60 * 24;
-	const days = Math.floor(diferencia / milisegundosEnDia);
-	const months = Math.floor(days / 30);
-	const years = Math.floor(months / 12);
-	console.log(years, months % 12, days);
-	$YearSpan.innerHTML = years;
-	$MonthSpan.innerHTML = months % 12;
-	$DaySpan.innerHTML = days % 30;
+	const ageDate = new Date(currentDate - userDate);
+	let yearDiff = currentDate.getFullYear() - userDate.getFullYear();
+	if (currentDate >= userDate) {
+		yearDiff--;
+	}
+	const monthDiff = ageDate.getMonth();
+	const dayDiff = ageDate.getDate() - 1;
+
+	const $P = document.querySelectorAll('.error');
+
+	if (!$DayInput.value) {
+		$P[0].classList.add('visible');
+		$DayInput.classList.add('border-error');
+		document.querySelector('.span-day-input').classList.add('color-error');
+	}
+	if (!$MonthInput.value) {
+		$P[1].classList.add('visible');
+		$MonthInput.classList.add('border-error');
+		document.querySelector('.span-month-input').classList.add('color-error');
+	}
+	if (!$YearInput.value) {
+		$P[2].classList.add('visible');
+		$YearInput.classList.add('border-error');
+		document.querySelector('.span-year-input').classList.add('color-error');
+	}
+
+	if ($DayInput.value && $MonthInput.value && $YearInput.value) {
+		$P.forEach((p) => {
+			p.classList.remove('visible');
+		});
+		$YearSpan.innerHTML = yearDiff;
+		$MonthSpan.innerHTML = monthDiff;
+		$DaySpan.innerHTML = dayDiff;
+	}
+};
+
+$Submit.addEventListener('click', () => {
+	calculateAge();
 });
